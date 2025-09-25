d1c58538ad222eeae697540a7f47438e7d812a22
C9BrenoFR <brenufr@gmail.com>
2025-09-25 02:17:08 -0300

    Redirecionamento para fora da home

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/api/documentation');
 });
 
 Route::get('/dashboard', function () {
